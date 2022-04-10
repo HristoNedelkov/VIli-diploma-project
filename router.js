e5bf39708074111f0eba4266afc3a452da79b965
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2022-04-10 17:42:35 +0300

    add about-us-page

--- router.js
+++ router.js
@@ -1,48 +1,51 @@
 const routes = {
-    'home': 'home-template',
-    'login': 'login-template',
-    'register': 'register-template',
-    'details': 'details-template',
-    'edit': 'edit-template',
-
-}
+  home: "home-template",
+  login: "login-template",
+  register: "register-template",
+  details: "details-template",
+  edit: "edit-template",
+  "about-us": "aboutUs-template",
+};
 
 const router = async (fullPath) => {
-    let [path, id] = fullPath.split('/')
+  let [path, id] = fullPath.split("/");
 
-    let app = document.getElementById('root')
-    let templateData = authServices.getData();
+  let app = document.getElementById("root");
+  let templateData = authServices.getData();
 
-    switch (path) {     //
-        case 'home':
-            let postsData = await itemSurvices.getAll()
-                .then(res => {
-                    if (Object.keys(res).length != 0) {
-                        templateData.posts = res
-                    }
-                })
-            break;
-        case 'logout':
-            authServices.logout()
-            return navigate('home')
-            break;
-        case 'details':
+  switch (
+    path //
+  ) {
+    case "home":
+      let postsData = await itemSurvices.getAll().then((res) => {
+        if (Object.keys(res).length != 0) {
+          templateData.posts = res;
+        }
+      });
+      break;
+    case "details":
+      let postDetails = await itemSurvices.getOne(id);
 
-            let postDetails = await itemSurvices.getOne(id)
+      console.log(postDetails);
+      Object.assign(templateData, postDetails);
+      break;
+    case "delete":
+      await itemSurvices.deleteOne(id);
+      navigate("home");
+      break;
+    case "logout":
+      authServices.logout();
+      return navigate("home");
+      break;
+    case "about-us":
+      console.log("We are currently in about-us page!");
+      break;
+    default:
+      break;
+  }
 
-            console.log(postDetails)
-            Object.assign(templateData, postDetails)
-            break;
-        case 'delete':
-            await itemSurvices.deleteOne(id)
-
-             navigate('home')
-            return
-                
-        default:
-            break;
-    }
-
-    let temp = Handlebars.compile(document.getElementById(routes[path]).innerHTML)
-    app.innerHTML = temp(templateData)
-}
+  let temp = Handlebars.compile(
+    document.getElementById(routes[path]).innerHTML
+  );
+  app.innerHTML = temp(templateData);
+};
