c2543a49fe6d090cc937f02001d99e91f91b0bac
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2022-05-28 20:30:31 +0300

    make it work only with js

--- router.js
+++ router.js
@@ -1,54 +1,26 @@
-const routes = {
-  home: "home-template",
-  login: "login-template",
-  register: "register-template",
-  details: "details-template",
-  "about-us": "aboutUs-template",
-  contact: "contact-template",
-  gallery: "gallery-template",
-  catalog: "catalog-template",
-  info: "info-template",
+let routes = {
+  home: document.getElementById("home-logged-template"),
+  "about-us": document.getElementById("aboutUs-template"),
+  contact: document.getElementById("contact-template"),
+  gallery: document.getElementById("gallery-template"),
+  catalog: document.getElementById("catalog-template"),
+  info: document.getElementById("info-template"),
 };
-
-const router = async (fullPath) => {
-  let [path, id] = fullPath.split("/");
- 
-  switch (path) {
-    case "info":
-      await itemSurvices.getAll().then((res) => {
-        if (Object.keys(res).length != 0) {
-          res.map((e) => {
-            e.content = e.content.substring(0, 100);
-            e.content += "...";
-          });
-          templateData.posts = res.reverse();
-        }
-      });
-      break;
-    case "contact":
-      await commentServices.getAll().then((res) => {
-        if (Object.keys(res).length != 0) templateData.comments = res;
-      });
-      break;
-    case "details":
-      let postDetails = await itemSurvices.getOne(id);
-      Object.assign(templateData, postDetails);
-      break;
-    case "delete":
-      await itemSurvices.deleteOne(id);
-      navigate("home");
-      break;
-    case "logout":
-      authServices.logout();
-      return navigate("home");
-      break;
-
-    default:
-      break;
+function show(...componentsToBeShown) {
+  for (const key in routes) {
+    if (componentsToBeShown.includes(key)) {
+      routes[key].style.display = "block";
+    } else {
+      routes[key].style.display = "none";
+    }
   }
+}
+function navigateHandler(event) {
+  event.preventDefault();
+  let url = new URL(event.target.href);
+  const path = url.pathname.slice(1);
 
-  let temp = Handlebars.compile(
-    document.getElementById(routes[path]).innerHTML
-  );
-  app.innerHTML = temp(templateData);
-};
+  // window.history.replaceState({}, "", "/" + path);
+  show(path);
+  //That returns us only the patch word
+}
